8b485d9923feb7b9cfd094dd5dc9ce425214114b
Zainal Hasan <zhanang19@gmail.com>
2021-03-26 14:29:36 +0700

    💄 Add custom styling for generated content

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,5 +1,5 @@
 <x-blog-layout>
-    <div class="mt-8 bg-white overflow-hidden prose prose-green max-w-none">
+    <div class="mt-8 bg-white overflow-hidden prose prose-green max-w-none post-content">
         {!! $post->content_markdown !!}
     </div>
 </x-blog-layout>
